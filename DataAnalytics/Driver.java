d8bac48e33f4304eddcd25e23a7ca9c52174121a
gryndanya <52318019+gryndanya@users.noreply.github.com>
2021-03-31 10:11:54 +0300

    Update Driver.java

--- DataAnalytics/Driver.java
+++ DataAnalytics/Driver.java
@@ -1,34 +1,48 @@
 package Project1;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Driver {
+	private static void initAndShowGUI() {
+
+		DummyPredictor tester = new DummyPredictor();
+		tester.readData("test.txt");
+		
+		String accuracy = "";
+		accuracy = accuracy + String.format("%.2f", tester.getAccuracy(tester.data)) + "% correct.";		
+		String precision = "";
+		precision = precision + String.format("%.2f", tester.getPrecision(tester.data))  + "% off target.";
+
+		JFrame myFrame = new JFrame();
+		myFrame.setTitle("DummyPredictor Stats");
+		myFrame.setLocation(500, 500);
+
+		Container contentPane = myFrame.getContentPane();
+		contentPane.setLayout(new GridLayout(2, 2));
+		contentPane.add(new Label("Accuracy: "));
+		contentPane.add(new Label(accuracy));
+		contentPane.add(new Label("Precision: "));
+		contentPane.add(new Label(precision));
+
+		myFrame.pack();
+		myFrame.setVisible(true);
+	}
+
 	public static void main(String[] args) {
-		DummyPredictor predictor = new DummyPredictor();
-		String token = "";
-		ArrayList<DataPoint> data = new ArrayList<DataPoint>();
-		data.add(new DataPoint());
-		try {
-			Scanner scanner = new Scanner(new File("text.txt"));
-			while (scanner.hasNextLine()) {
-				token = (scanner.nextLine());
-				System.out.println(token);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				initAndShowGUI();
 			}
-			scanner.close();
-		} catch (FileNotFoundException ex) {
-			System.out.println("File Not Found");
-		}
-		
-		DataPoint testData = new DataPoint(1.0);
-		
-		predictor.readData(data);
-		System.out.println(predictor.test(testData)); 
+		});
 
 	}
-	}
+}
 
 
 
